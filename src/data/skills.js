5a2c712c5c821de3fd1613746a182d234ca13f33
Brian Jamiel <brianthomasjamiel@gmail.com>
2026-02-17 20:00:47 -0800

    swapped skills for the skills from the manual

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,48 +1,32 @@
 export const SKILL_CATEGORIES = [
   {
-    id: "warfare",
-    name: "Warfare",
+    id: "affinity",
+    name: "INFLUENCE FOR...",
     skills: [
-      { id: "brawl", name: "Brawl", stat: "PHY" },
-      { id: "ballistics", name: "Ballistics", stat: "COG" },
-      { id: "tactics", name: "Tactics", stat: "COG" },
-      { id: "weaponry", name: "Melee Weaponry", stat: "PHY" }
+      { id: "crafts/melee slashing", name: "A Better Price On A Sword", stat: "PHY" },
+      { id: "genesis tech", name: "Be Given An Genesis Artifact", stat: "COG" },
+      { id: "navigate/dominions", name: "A Hidden Location", stat: "COG" },
+      { id: "emergency aid/medicine", name: "Getting Help By A Doctor", stat: "PHY" }
     ]
   },
   {
     id: "movement",
-    name: "Movement",
+    name: "RUNNING...",
     skills: [
-      { id: "maneuver", name: "Maneuver", stat: "SPD" },
-      { id: "stealth", name: "Stealth", stat: "SPD" },
-      { id: "athletics", name: "Athletics", stat: "PHY" }
+      { id: "stealth", name: "Quietly from Pursuers", stat: "SPD" },
+      { id: "pantheon", name: "Up The Back Of A Monster", stat: "SPD" },
+      { id: "listen/sixth sense", name: "Through A Pitch Black Forest", stat: "PHY" },
+      { id: "psychology/disguise", name: "Without Attracting Attention", stat: "COG"}
     ]
   },
   {
-    id: "arcane",
-    name: "Arcane Arts",
+    id: "perception",
+    name: "SEARCHING FOR...",
     skills: [
-      { id: "channel", name: "Channeling", stat: "SPR" },
-      { id: "lore", name: "Arcane Lore", stat: "COG" },
-      { id: "willpower", name: "Willpower", stat: "SPR" }
-    ]
-  },
-  {
-    id: "tech",
-    name: "Technology",
-    skills: [
-      { id: "hacking", name: "Hacking", stat: "COG" },
-      { id: "engineering", name: "Engineering", stat: "COG" },
-      { id: "piloting", name: "Piloting", stat: "DRV" }
-    ]
-  },
-  {
-    id: "social",
-    name: "Influence",
-    skills: [
-      { id: "persuasion", name: "Persuasion", stat: "CHA" },
-      { id: "deception", name: "Deception", stat: "CHA" },
-      { id: "intimidation", name: "Intimidation", stat: "CHA" }
+      { id: "arcane arts/dominions", name: "Arcane Books", stat: "SPR" },
+      { id: "sixth sense/lock pick", name: "A Hidden Door", stat: "COG" },
+      { id: "maneuver/stealth", name: "Hidden Movements", stat: "SPR" },
+      { id: "melee warfare", name: "A Brute Lost In The City", stat: "PHY"}
     ]
   }
 ];
