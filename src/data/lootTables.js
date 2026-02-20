9b39096cfa817a8ea0ecf9c5fe9c7dee926efe12
61ZM07H36R3ML1N <brianthomasjamiel@gmail.com>
2026-02-20 08:31:26 -0800

    Start of Loot Tables

---
+++ src/data/lootTables.js
@@ -0,0 +1,8 @@
+// src/data/lootTables.js
+
+export const LOOT_CATEGORIES = [
+  { min: 1, max: 4, type: "Valuables" },
+  { min: 5, max: 11, type: "Consumables" },
+  { min: 12, max: 15, type: "Vestiges" },
+  { min: 16, max: 20, type: "Gear" }
+];
